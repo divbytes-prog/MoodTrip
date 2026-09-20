1d105532d9024aeb200b84190e4c08ed3cb0e16b
divbytes-prog <24dcs032@lnmiit.ac.in>
2026-09-20 22:22:39 +0530

    Add standalone MoodTrip entry point

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import MoodTrip from './MoodTrip.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MoodTrip />
+  </React.StrictMode>
+);
